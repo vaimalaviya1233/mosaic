50dab9800fb1f71078dcf01b28e33f573c02cfc0
Jake Wharton <jw@squareup.com>
2024-08-20 22:03:23 -0400

    API dump & allow raw mode to fail

--- mosaic-runtime/src/inputMain/kotlin/com/jakewharton/mosaic/input.kt
+++ mosaic-runtime/src/inputMain/kotlin/com/jakewharton/mosaic/input.kt
@@ -2,12 +2,13 @@ package com.jakewharton.mosaic
 
 import com.github.ajalt.mordant.input.MouseTracking
 import com.github.ajalt.mordant.input.RawModeScope
-import com.github.ajalt.mordant.input.enterRawMode
+import com.github.ajalt.mordant.input.enterRawModeOrNull
 import com.github.ajalt.mordant.terminal.Terminal
 import kotlin.time.Duration
 
 internal actual fun Terminal.enterRawMode(): RawMode? {
-	return MordantRawMode(enterRawMode(MouseTracking.Off))
+	return enterRawModeOrNull(MouseTracking.Off)
+		?.let(::MordantRawMode)
 }
 
 private class MordantRawMode(
